439ac47688c312e00b26762f4a9b9adf894e9fb9
ant22boss <ant22boss@gmail.com>
2018-11-02 12:03:40 +0800

    frpc add

---
+++ frpc/version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FRPC_VER=0.20.0
+export FRPC_INIT="https://raw.githubusercontent.com/hebskjcc/onekey-install-shell/master/frpc/frpc.init"
+export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
+export github_download_url="https://github.com/hebskjcc/frp/releases/download"
