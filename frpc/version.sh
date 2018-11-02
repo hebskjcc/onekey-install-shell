6606737f9f5efda5db97cd784c44fb098a442e5a
hebskjcc <hebskjcc@gmail.com>
2018-11-02 14:37:03 +0800

    check bug

--- frpc/version.sh
+++ frpc/version.sh
@@ -2,4 +2,4 @@
 export FRPC_VER=0.20.0
 export FRPC_INIT="https://raw.githubusercontent.com/hebskjcc/onekey-install-shell/master/frpc/frpc.init"
 export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
-export github_download_url="https://github.com/hebskjcc/frp/releases/download"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
